2223c2a6800545c1de708073cf90c45b01a2988d
AnshumAshu <anshumashu2002@gmail.com>
2025-06-19 16:31:54 +0530

    today2 commit

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ const connectDB = require('./config/db');
 require('dotenv').config();
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
